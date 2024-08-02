00a97a2a7eca1d4824a6d3064fa34cd14ea54393
nelg62 <159752496+nelg62@users.noreply.github.com>
2024-08-02 16:08:46 +1200

    Update astro.config.mjs

--- PortfolioWebsite/astro.config.mjs
+++ PortfolioWebsite/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://nelg62.github.io/',
-  base: '/PortfolioWebsite/src',
+  base: '/PortfolioWebsite/',
 });
