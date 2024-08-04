b595370ddfb2b9ecb9d4cbcbeb010944e229fe2f
nelg62 <glen.harding.nz@gmail.com>
2024-08-05 11:17:39 +1200

    starting porftolio again with typescript after astro tutorial

--- PortfolioWebsite/astro.config.mjs
+++ PortfolioWebsite/astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://nelg62.github.io/',
-  base: '/PortfolioWebsite/',
-});
+export default defineConfig({});
