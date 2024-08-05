6d36bfcc010525f7ba27bf286b93c4fdaefa21f0
nelg62 <glen.harding.nz@gmail.com>
2024-08-05 14:28:03 +1200

    start agian

--- PortfolioWebsite/astro.config.mjs
+++ PortfolioWebsite/astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
-import rss from "@astrojs/rss";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://nelg62.github.io/",
   base: "/PortfolioWebsite/",
-  integrations: [preact(), rss()],
 });
