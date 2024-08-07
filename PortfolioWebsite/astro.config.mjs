e844c1aeea03193d7e21c0784a94416db599b716
nelg62 <glen.harding.nz@gmail.com>
2024-08-07 12:39:34 +1200

    portfolio looking good so far but more is needed

--- PortfolioWebsite/astro.config.mjs
+++ PortfolioWebsite/astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nelg62.github.io",
   base: "/PortfolioWebsite",
+  integrations: [tailwind(), react()]
 });
