452881ccbd1cc6c174510c445b3dc775c8275b04
nelg62 <glen.harding.nz@gmail.com>
2024-08-12 12:01:56 +1200

    add cv and tidy slightly

--- PortfolioWebsite/src/lib/constants.ts
+++ PortfolioWebsite/src/lib/constants.ts
@@ -25,10 +25,9 @@ export const LINKS = {
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION:
-    "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "",
+  DESCRIPTION: "",
+  AUTHOR: "",
 };
 
 // Work Page
