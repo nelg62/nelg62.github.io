272fd9eaba43cf0c3e35df47f76db06af6c0b688
nelg62 <glen.harding.nz@gmail.com>
2024-10-14 17:21:45 +1300

    into and about me cards basic created will need to fill out info and mabie style a bit more

---
+++ PortfolioWebsite/src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const ME = {
+  firstName: "Glen",
+  fullName: "Glen Harding",
+  country: "New Zealand",
+  email: "glen.harding.nz@gmail.com",
+};
+
+export const LINKS = {
+  github: "https://github.com/nelg62",
+  linkedin: "https://www.linkedin.com/in/glen-harding-5a1317114",
+  mail: "mailto:glen.harding.nz@gmail.com",
+  thisProject: "https://github.com/nelg62/nelg62.github.io",
+  // instagram: "",
+  // medium: "",
+  // discord: "",
+};
